ff20e63dd2a2e2e26f0d272d91e780e0755eb6b3
majjikishore <53621314+majjikishore007@users.noreply.github.com>
2020-10-07 15:18:02 +0530

    added out put

--- src/Q9.java
+++ src/Q9.java
@@ -1,40 +1,28 @@
-/**
- * 
- * @author kishoremajji007
- *Special Pythagorean triplet
+/**Que Special Pythagorean triplet
+ * output 31875000
+ * @author kishoremajji007 
  */
 public class Q9 {
-	public static void getPythagoreantriplet(long n) {
+	public static long getPythagoreantriplet(long n) {
+		long ans=0;
 		for (long a = 3; a <= n; a++) {
-            for (long b = a + 1; b < n; b++) {
-                
-                double cS =  (Math.pow(a, 2) + Math.pow(b, 2));
-                double c = Math.pow(cS, 0.5);
-                
-                if (a + b + c == n) {
-                    System.out.println(a * b * c);
-                    break;
-                }
-            }
-        }
+			for (long b = a + 1; b < n; b++) {
+
+				double cS = (Math.pow(a, 2) + Math.pow(b, 2));
+				double c = Math.pow(cS, 0.5);
+
+				if (a + b + c == n) {
+					
+				 ans=(long) (a*b*c);
+				}
+			}
+		}
+		return ans;
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		getPythagoreantriplet(1000);
-		for (int a = 3; a <= 1000; a++) {
-            for (int b = a + 1; b < 1000; b++) {
-                
-                double cSquared = Math.pow(a, 2) + Math.pow(b, 2);
-                double c = Math.pow(cSquared, 0.5);
-                
-                if (a + b + c == 1000) {
-                	System.out.println(a+" "+b+" "+c);
-                	double ans=a*b*c;
-                	System.out.println(ans);
-                    break;
-                }
-            }
-        }
+		System.out.println(getPythagoreantriplet(1000));
 	}
 
 }
